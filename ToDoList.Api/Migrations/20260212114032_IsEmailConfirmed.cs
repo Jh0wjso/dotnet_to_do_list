25f2ed97238d49ad2f428ce8da36fd2c17a68108
Jh0wjso <jhonatansilverio1258@gmail.com>
2026-02-12 08:57:50 -0300

    Migration for user email verification

---
+++ ToDoList.Api/Migrations/20260212114032_IsEmailConfirmed.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsEmailConfirmed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailConfirmationToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmailConfirmationTokenExpiry",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailConfirmed",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmationToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmationTokenExpiry",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailConfirmed",
+                table: "Users");
+        }
+    }
+}
